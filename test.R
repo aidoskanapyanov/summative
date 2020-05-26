54e43422f4b8ad019d0c2bb75593041fd8dec1b0
aidoskanapyanov <aidos.kanapyanov@nu.edu.kz>
2020-05-27 02:25:25 +0600

    tired af

--- test.R
+++ test.R
@@ -16,3 +16,30 @@ microbenchmark(loop <- Factorial_loop(20),
                reduce <- Factorial_reduce(20),
                func <- Factorial_func(20),
                memo <- Factorial_mem(20))
+####################################################
+
+
+
+
+data <- read_csv("data/MIE.csv")
+
+make_LD <- function(x){
+    structure(list(id = x[1], visit = x[2], room = x[3],
+                   value = x[4], timepoint = x[5]), class = "LongitudinalData")
+}
+
+print.LongitudinalData <- function(x){
+    print(paste("Longitudinal dataset with", nrow(unique(x$id)),"subjects"))
+}
+
+subject.LongitudinalData <- function(object, id) {
+    
+}
+
+x <- make_LD(data)
+print(class(x))
+print(x)
+
+## Subject 10 doesn't exist
+out <- subject(x, 10)
+print(out)
